5062cf2318f4d2a570d8af1dc43d6528646c9839
stolpovski <stolpovski@gmail.com>
2022-04-07 15:39:55 +0300

    added blockchain

--- app.js
+++ app.js
@@ -10,6 +10,58 @@ class Block{
     }
 
     calculateHash(){
-        return SHA256(JSON.stringify(this.data)).toString();
+        return SHA256(
+            this.index
+            + this.previousHash
+            + this.timestamp
+            + JSON.stringify(this.data)
+        ).toString();
     }
 }
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock(){
+        return new Block(0, '01/01/2022', 'Genesis block', '0');
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+    
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let coin = new Blockchain();
+coin.addBlock(new Block(1, '01/01/2022', { amount: 4 }));
+coin.addBlock(new Block(2, '02/01/2022', { amount: 10 }));
+
+// console.log(JSON.stringify(coin, null, 4));
+console.log('Is blockchain valid? ' + coin.isChainValid());
+
+coin.chain[1].data = { amount: 100 };
+console.log('Is blockchain valid? ' + coin.isChainValid());
