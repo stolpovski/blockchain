eb65a0022a94e792668cd005f24bb11b28337a4c
stolpovski <stolpovski@gmail.com>
2022-04-07 13:36:41 +0300

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash(){
+        return SHA256(JSON.stringify(this.data)).toString();
+    }
+}
